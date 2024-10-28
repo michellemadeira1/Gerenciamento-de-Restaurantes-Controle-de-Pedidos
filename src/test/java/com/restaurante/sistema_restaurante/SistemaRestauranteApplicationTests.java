df7f3a4c9fa7b865d49838d9a7acf5fa2dbca735
Michelle Madeira <michellemadeira1@gmail.com>
2024-10-28 12:50:40 -0300

    Criação projeto

---
+++ src/test/java/com/restaurante/sistema_restaurante/SistemaRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurante.sistema_restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
